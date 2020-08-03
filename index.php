3723fb274ca1ab1b43a048a043a1128986eedf71
= <=>
2020-08-03 15:21:45 -0500

    style added

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <html>
+
+<head>
+  <link rel="stylesheet" href="style.css">
+</head>
+
 <body>
 
 <form action="db_connection.php" method="post">
-Name: <input type="text" name="name"><br>
-Last Name: <input type="text" name="lastname"><br>
-E-mail: <input type="email" name="email"><br>
+<label>Name</label><br>
+<input type="text" name="name"><br>
+<label>Last Name</label><br>
+<input type="text" name="lastname"><br>
+<label>E-mail</label><br>
+<input type="email" name="email"><br>
 <input type="submit">
 </form>
 
